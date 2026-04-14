fcb44203d6620ae01162c34e4593013867cf61c4
Manuel Barros <manuelbarros286@gmail.com>
2026-04-14 11:19:46 +0100

    handle automated database migration and tested as working in Scalar's API-suite

--- CareerlotAPI/Program.cs
+++ CareerlotAPI/Program.cs
@@ -26,6 +26,21 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 app.UseCors("VitePolicy");
 
 if (app.Environment.IsDevelopment())
